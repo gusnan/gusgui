5953a1cf6d14de8ca42ffe5be0120a6d46538008
Andreas Rönnquist <gusnan@gusnan.se>
2011-05-10 15:45:50 +0200

    Make all headers LGPL, GusGui copyright me 2011

--- examples/example4.cpp
+++ examples/example4.cpp
@@ -1,20 +1,20 @@
 /**
  *
- *	This file is part of GusGame.
+ *	This file is part of GusGui.
  *	Copyright 2011 Andreas Rönnquist
  *
- *	GusGame is free software: you can redistribute it and/or modify
+ *	GusGui is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU Lesser General Public License as published by
  *	the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
  *
- *	GusGame is distributed in the hope that it will be useful,
+ *	GusGui is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *	GNU Lesser General Public License for more details.
  *
  *	You should have received a copy of the GNU Lesser General Public License
- *	along with GusGame.  If not, see <http://www.gnu.org/licenses/>.
+ *	along with GusGui.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #include <cstdlib>
