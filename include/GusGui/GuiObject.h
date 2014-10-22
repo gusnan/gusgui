c0ad1d3eb97aa6b342db6642969952bfe87b9659
Andreas Rönnquist <gusnan@gusnan.se>
2014-10-22 06:35:26 +0200

    Update copyright header

--- include/GusGui/GuiObject.h
+++ include/GusGui/GuiObject.h
@@ -1,19 +1,19 @@
 /**
- * This file is part of Damnation of the Gods
- * Copyright 2008,2009 Andreas Rönnquist
+ * This file is part of GusGui
+ * Copyright 2008-2014 Andreas Rönnquist
  *
- * Damnation of the Gods is free software: you can redistribute it and/or
+ * GusGui is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Damnation of the Gods is distributed in the hope that it will be useful,
+ * GusGui is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Damnation of the Gods.
+ * along with GusGui.
  * If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __HEADER_GUI_OBJECT_
--- src/GuiObjects/GuiObject.cpp
+++ src/GuiObjects/GuiObject.cpp
@@ -1,21 +1,20 @@
 /**
+ * This file is part of GusGui
+ * Copyright 2008-2014 Andreas Rönnquist
  *
- *	This file is part of GusGui.
- *	Copyright 2011 Andreas Rönnquist
+ * GusGui is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *	GusGui is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU Lesser General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ * GusGui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *	GusGui is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU Lesser General Public License for more details.
- *
- *	You should have received a copy of the GNU Lesser General Public License
- *	along with GusGui.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with GusGui.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <boost/shared_ptr.hpp>
