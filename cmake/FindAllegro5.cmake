a3ae221cebe027fb4cef52ae08410351871a7e7f
Andreas Rönnquist <gusnan@openmailbox.org>
2017-05-28 21:52:00 +0200

    Use Allegro 5.2

--- cmake/FindAllegro5.cmake
+++ cmake/FindAllegro5.cmake
@@ -11,21 +11,21 @@ IF (ALLEGRO_INCLUDE_DIR)
   SET(ALLEGRO_FIND_QUIETLY TRUE)
 ENDIF (ALLEGRO_INCLUDE_DIR)
 
-FIND_PATH(ALLEGRO_INCLUDE_DIR allegro.h
+FIND_PATH(ALLEGRO_INCLUDE_DIR allegro.h allegro5.h
   /usr/local/include/allegro5
   /usr/include/allegro5
   $ENV{MINGDIR}/include/allegro5
 )
 
-set(VERSION_NUMBER "5.1")
+set(VERSION_NUMBER "5")
 
 if(UNIX AND NOT CYGWIN)
 	exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARIES)
-	IF (NOT ${ALLEGRO_LIBRARIES})
+	IF (NOT ALLEGRO_LIBRARIES)
 	    # FALL BACK TO 5.0
 	    set(VERSION_NUMBER "5.0")
 	    exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARIES)
-	ENDIF (NOT ${ALLEGRO_LIBRARIES})
+	ENDIF (NOT ALLEGRO_LIBRARIES)
 	# Check if any of these checks have found if so then cause failure)
 	STRING(REGEX MATCH "not found" LIB_CHECK ${ALLEGRO_LIBRARIES})
 	IF (${LIB_CHECK})
